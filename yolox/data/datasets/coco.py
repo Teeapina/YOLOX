570cb7b62b5fa56495d05911cbbebd8eb8e9bc4a
Teeapina <joel.toppinen@jcad.fi>
2025-06-24 12:55:44 +0300

    Update coco.py

--- yolox/data/datasets/coco.py
+++ yolox/data/datasets/coco.py
@@ -60,7 +60,7 @@ class COCODataset(CacheDataset):
         self.data_dir = data_dir
         self.json_file = json_file
 
-        self.coco = COCO(os.path.join(self.data_dir, "annotations", self.json_file))
+        self.coco = COCO(os.path.join(self.data_dir, self.json_file))
         remove_useless_info(self.coco)
         self.ids = self.coco.getImgIds()
         self.num_imgs = len(self.ids)
