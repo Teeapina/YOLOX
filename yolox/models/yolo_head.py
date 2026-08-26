7f3edbd6b5f8c8f9820d226ced13a78b8346ba0f
Teeapina <joel.toppinen@jcad.fi>
2026-08-26 09:45:05 +0300

    Clamp dynamic_ks to max cost shape

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -546,7 +546,9 @@ class YOLOXHead(nn.Module):
 
         n_candidate_k = min(10, pair_wise_ious.size(1))
         topk_ious, _ = torch.topk(pair_wise_ious, n_candidate_k, dim=1)
-        dynamic_ks = torch.clamp(topk_ious.sum(1).int(), min=1)
+        dynamic_ks = torch.clamp(
+            topk_ious.sum(1).int(), min=1, max=cost.shape[1]
+        )
         for gt_idx in range(num_gt):
             _, pos_idx = torch.topk(
                 cost[gt_idx], k=dynamic_ks[gt_idx], largest=False
